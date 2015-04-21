c75254475bacdfd88de72db2d494cc6205483980
Diego Mazzone <mazzone.diego@gmail.com>
2015-04-21 20:24:49 -0300

    Initial Commit

---
+++ ScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView
+//
+//  Created by Diego Mazzone on 21/4/15.
+//  Copyright (c) 2015 Mundo Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
